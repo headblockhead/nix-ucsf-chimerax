366d9a13ece39e47e3cb1e5809ce4a7d4378a97a
Ryan Swart <ryan@talo.systems>
2022-10-25 16:54:46 +0800

    fix: find all required libraries
    By using the deb file instead of the 1.4 release, we can get all of the libraries to resolve. However, they include a bunch of files in python site-packages with whitespace in their name, which nix is unhappy with

--- chimerax.nix
+++ chimerax.nix
@@ -1,86 +1,101 @@
-{ pkgs, stdenv, fetchurl, autoPatchelfHook }:
+{ pkgs, stdenv, dpkg, glibc, gcc-unwrapped, autoPatchelfHook }:
+let
 
-let 
+  # Please keep the version x.y.0.z and do not update to x.y.76.z because the
+  # source of the latter disappears much faster.
+  version = "rc";
+
+  src = ./chimerax-rc.deb;
+  libnsl = stdenv.mkDerivation rec {
+    pname = "libnsl";
+    version = "1.3.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "thkukuk";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "1dayj5i4bh65gn7zkciacnwv2a0ghm6nn58d78rsi4zby4lyj5w5";
+    };
+
+    nativeBuildInputs = [ pkgs.autoreconfHook pkgs.pkg-config ];
+    buildInputs = [ pkgs.libtirpc ];
+  };
   my-python-packages = python-packages: with python-packages; [
     webencodings
-  ]; 
+  ];
   python-with-my-packages = pkgs.python39.withPackages my-python-packages;
 in
-stdenv.mkDerivation {
-  pname = "chimerax";
-  version = "1.4";
-  src = fetchurl {
-#    url =
-#      "https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?ident=OHeQer2fSLp7%2BfxtoHxc5%2Flkr0FSQ9j90xJ03g75hA%3D%3D&file=1.4%2Flinux%2FChimeraX-1.4.tar.gz&choice=Notified";
-    url =
-      "https://www.rbvi.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?ident=OHeQer2fSLp7%2BfxtoHxc5%2Flkr0FSQ9j%2B0xtz0gr5gvM%3D&file=1.4%2Flinux%2FChimeraX-1.4.tar.gz&choice=Notified";
-    sha256 = "00JVND2G+46IkjApDs837C2kLxLV9IZATCDE3mNRfIc=";
-  };
+stdenv.mkDerivation rec {
+  name = "chimerax-${version}";
+
+  system = "x86_64-linux";
+
+  inherit src;
+
+  # Required for compilation
+  nativeBuildInputs = [
+    autoPatchelfHook # Automatically setup the loader, and do the magic
+    pkgs.addOpenGLRunpath
+    pkgs.cudaPackages.autoAddOpenGLRunpathHook
+    pkgs.qt6.wrapQtAppsHook
+    dpkg
+  ];
+
+  # Required at running time
   buildInputs = [
+    glibc
+    gcc-unwrapped
+    pkgs.linuxKernel.packages.linux_5_19.nvidia_x11
     pkgs.libffi
-          pkgs.qt6.wrapQtAppsHook
-          pkgs.glib
-          pkgs.gdk-pixbuf
-          pkgs.cairo
-          pkgs.pango
-          pkgs.udev
-          pkgs.alsa-lib
-          pkgs.gtk3
-          pkgs.webkitgtk
-          pkgs.pkg-config
-          pkgs.xorg.libX11
-          pkgs.xorg.libXcursor
-          pkgs.xorg.libXrandr
-          pkgs.gcc-unwrapped
-          pkgs.ffmpeg 
-          pkgs.qt4
-          pkgs.qt6.qt3d
-          pkgs.qt6.qtquick3d
-          pkgs.qt6.qtwebview
-          pkgs.opencl-info
-          pkgs.ncurses
-          pkgs.opencl-clang
-          pkgs.opencl-headers
-          pkgs.rocm-opencl-runtime
-          pkgs.opencl-clhpp
-          pkgs.conda
-          pkgs.vial
-          pkgs.mysql80
-          python-with-my-packages
-        ];
-        propagatedBuildInputs = [
-          pkgs.libffi
-           pkgs.qt6.wrapQtAppsHook
-          pkgs.glib
-          pkgs.gdk-pixbuf
-          pkgs.cairo
-          pkgs.pango
-          pkgs.udev
-          pkgs.alsa-lib
-          pkgs.gtk3
-          pkgs.webkitgtk
-          pkgs.pkg-config
-          pkgs.xorg.libX11
-          pkgs.xorg.libXcursor
-          pkgs.xorg.libXrandr
-          pkgs.gcc-unwrapped
-          pkgs.ffmpeg 
-          pkgs.qt4
-          pkgs.qt6.qt3d
-          pkgs.qt6.qtquick3d
-          pkgs.qt6.qtwebview
-          pkgs.opencl-info
-          pkgs.ncurses
-          pkgs.opencl-clang
-          pkgs.opencl-headers
-          pkgs.rocm-opencl-runtime
-          pkgs.opencl-clhpp
-          pkgs.conda
-          pkgs.vial
-          pkgs.mysql80
-          python-with-my-packages       
-        ];
-  nativeBuildInputs = [ autoPatchelfHook ];
-  installPhase = ./chimerax-install.sh;
-  system = builtins.currentSystem;
+    pkgs.qt6.wrapQtAppsHook
+    pkgs.glib
+    pkgs.gdk-pixbuf
+    pkgs.cairo
+    pkgs.pango
+    pkgs.udev
+    pkgs.alsa-lib
+    pkgs.gtk3
+    pkgs.webkitgtk
+    pkgs.pkg-config
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.gcc-unwrapped
+    pkgs.ffmpeg
+    pkgs.qt4
+    pkgs.qt6.qt3d
+    pkgs.qt6.qtquick3d
+    pkgs.qt6.qtwebview
+    pkgs.opencl-info
+    pkgs.ncurses
+    pkgs.libtirpc
+    libnsl
+    pkgs.opencl-clang
+    pkgs.opencl-headers
+    pkgs.rocm-opencl-runtime
+    pkgs.opencl-clhpp
+    pkgs.conda
+    pkgs.vial
+    pkgs.mysql80
+    pkgs.cudaPackages.cudatoolkit
+    pkgs.openssl_3
+    python-with-my-packages
+  ];
+  propagatedBuildInputs = buildInputs;
+  unpackPhase = "true";
+
+  # Extract and copy executable in $out/bin
+  installPhase = ''
+    mkdir -p $out
+    dpkg -x $src $out
+    cp -av $out/usr/* $out
+    rm -rf $out/usr
+  '';
+
+  # meta = with stdenv.lib; {
+  #   description = "ChimeraX";
+  #   homepage = https://www.cgl.ucsf.edu/chimerax/;
+  #   license = licenses.mit;
+  #   maintainers = with stdenv.lib.maintainers; [ ];
+  #   platforms = [ "x86_64-linux" ];
+  # };
 }
